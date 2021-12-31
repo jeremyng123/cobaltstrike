51a67ec918904d8a3b7ea07ab62a10b58183eb8f
Jeremy <ngyzj95@gmail.com>
2021-12-31 11:46:31 +0800

    made changes to scan to import and run script to scan batch IPs

--- scan.py
+++ scan.py
@@ -2,7 +2,91 @@ import argparse
 import requests
 import urllib3
 from urllib.parse import urljoin
-from lib import decrypt_beacon, decode_config, JsonEncoder
+from .lib import decrypt_beacon, decode_config, JsonEncoder
+
+def tek_scan(host):
+    ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    if not host.startswith("http"):
+        host = "http://{}/".format(host)
+        print("Assuming that you meant {}".format(host))
+
+    print("Checking {}".format(host))
+    try:
+        r = requests.get(urljoin(host, "/aaa9"), headers={'user-agent': ua}, verify=False, timeout=5)
+        if r.status_code == 200:
+            data = r.content
+            if data.startswith(b"\xfc\xe8"):
+                beacon = decrypt_beacon(data)
+                if beacon:
+                    config = decode_config(beacon)
+                    if config:
+                        print("Configuration of the x86 payload:")
+                        for d in config:
+                            if isinstance(config[d], bytearray):
+                                print("{:30} {}".format(d, config[d].hex()))
+                            else:
+                                print("{:30} {}".format(d, config[d]))
+                        print("")
+                    else:
+                        print("x86: Impossible to extract the configuration")
+                else:
+                    print("x86: Impossible to extract beacon")
+            elif data.startswith(b"MZ"):
+                # Sometimes it returns a PE directly
+                config = decode_config(data)
+                if config:
+                    print("Configuration of the x86 payload")
+                    for d in config:
+                        if isinstance(config[d], bytearray):
+                            print("{:30} {}".format(d, config[d].hex()))
+                        else:
+                            print("{:30} {}".format(d, config[d]))
+                    print("")
+                else:
+                    print("x86: Impossible to extract the configuration")
+            else:
+                print("x86: Payload not found")
+        else:
+            print("x86: HTTP Status code {}".format(r.status_code))
+
+        r = requests.get(urljoin(host, "aab9"), headers={'user-agent': ua}, verify=False, timeout=5)
+        if r.status_code == 200:
+            data = r.content
+            if data.startswith(b"\xfc\xe8"):
+                beacon = decrypt_beacon(data)
+                if beacon:
+                    config = decode_config(beacon)
+                    if config:
+                        for d in config:
+                            print("Configuration of the x86_64 payload")
+                            if isinstance(config[d], bytearray):
+                                print("{:30} {}".format(d, config[d].hex()))
+                            else:
+                                print("{:30} {}".format(d, config[d]))
+                    else:
+                        print("x86_64: Impossible to extract the configuration")
+                else:
+                    print("x86_64: Impossible to extract beacon")
+            elif data.startswith(b"MZ"):
+                # Sometimes it returns a PE directly
+                config = decode_config(data)
+                if config:
+                    print("Configuration of the x86_64 payload:")
+                    for d in config:
+                        if isinstance(config[d], bytearray):
+                            print("{:30} {}".format(d, config[d].hex()))
+                        else:
+                            print("{:30} {}".format(d, config[d]))
+                    print("")
+                else:
+                    print("x86_64: Impossible to extract the configuration")
+            else:
+                print("x86_64: Payload not found")
+        else:
+            print("x86_64: HTTP Status code {}".format(r.status_code))
+    except (requests.exceptions.ChunkedEncodingError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout, requests.exceptions.TooManyRedirects, requests.exceptions.ContentDecodingError):
+        print("Request failed")
 
 
 if __name__ == "__main__":
